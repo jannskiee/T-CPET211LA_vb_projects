157635e85a67dbfa1a1c897500ac8dc9a8badffb
Jc Paredes <paredesjancarlo99@gmail.com>
2024-06-12 18:15:29 +0800

    Add files via upload

---
+++ Emotions/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = ""
+        Label1.Font = New Font("Wingdings", 170)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = "J"
+        Label1.ForeColor = Color.Yellow
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label1.Text = "K"
+        Label1.ForeColor = Color.Blue
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Label1.Text = "L"
+        Label1.ForeColor = Color.Red
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
